a81e890b44cf5121be7b6a021b02f3c51de50779
LiAn1996 <953653973@qq.com>
2019-07-16 20:46:35 +0800

    ______

---
+++ gittest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("this is githud\n");
+	printf("hello  githud-----\n");
+	printf("_________________\n");
+	return 0;
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(int argc, const char *argv[])
+{
+	printf("asdasdaa\n");
+	return 0;
+}
